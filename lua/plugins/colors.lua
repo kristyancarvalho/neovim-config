79f7dec9260e2615a47d4a44e2707f271c197f2f
kristyancarvalho <kristyancarvalho@hotmail.com>
2025-06-14 18:27:32 -0300

    set transparent background.

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -4,6 +4,13 @@ return {
     priority = 1000,
     lazy = false,
     config = function()
-        vim.cmd.colorscheme("oxocarbon")
+        vim.cmd.colorscheme "oxocarbon"
+        vim.cmd [[
+            highlight Normal guibg=NONE ctermbg=NONE
+            highlight NormalNC guibg=NONE ctermbg=NONE
+            highlight EndOfBuffer guibg=NONE ctermbg=NONE
+            highlight LineNr guibg=NONE ctermbg=NONE
+            highlight SignColumn guibg=NONE ctermbg=NONE
+        ]]
     end,
 }
